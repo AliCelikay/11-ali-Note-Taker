63132199a272ff53fea29fb47e29da02f0dc6792
AliCelikay <111723837+AliCelikay@users.noreply.github.com>
2022-10-01 11:42:31 -0500

    Backend index and some bug fixes

---
+++ Develop/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+// Import our modular routers for
+
+
+const app = express();
+
+
+
+module.exports = app;
--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,32 @@
+//grabbing packages
 const express = require('express');
 const path = require('path');
+//Backend index.js package
+const api = require('./routes/index.js');
 
-const port = 3001;
+//creating a ports, 1 for heroku and one preset port
+const PORT = process.env.PORT || 3001;
 
+//grabbing experss function from the express package
 const app = express();
 
+//middleware for interpretation of user input and protection
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//
+app.use('/api/notes', api);
+
+//the public folder will be static
 app.use(express.static('public'));
+
 
 //Get route for homepage
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/'))
+    res.sendFile(path.join(__dirname, '/public'))
 });
 
+//route listens to the port in terminal
 app.listen(PORT, () => {
-    console.log("App listening at http://localhost:${PORT} 🚀");
+    console.log(`App listening at http://localhost:${PORT} 🚀`);
 })
