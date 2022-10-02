d19fc3475e112f5ed5b5e144de21fe9050cd5a3f
AliCelikay <111723837+AliCelikay@users.noreply.github.com>
2022-10-02 12:29:43 -0500

    Post routes written, works on Insom but not google

--- Develop/routes/notesRouter.js
+++ Develop/routes/notesRouter.js
@@ -1,13 +1,9 @@
-// const router = require('express').Router();
-// const path = require('path');
+const NotesRouter = require('express').Router();
+const path = require('path');
 
-// // GET Route for retrieving notes.html
+// GET Route for retrieving notes.html
+NotesRouter.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+});
 
-// router.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/index.html'))
-// });
-
-// router.get('/notes', (req, res) => {
-//   res.sendFile(path.join(__dirname, './public/notes.html'))
-// });
-// module.exports = router;
+module.exports = NotesRouter;
--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,11 @@
 //grabbing packages
 const express = require('express');
 const path = require('path');
+//
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile } = require('./helpers/fsUtils');
 //Backend index.js package
-const api = require('./routes/index.js');
-// const notesRouter = require('./routes/notesRouter.js');
+// const api = require('./routes/index.js');
 
 //creating a ports, 1 for heroku and one preset port
 const PORT = process.env.PORT || 3001;
@@ -27,9 +29,48 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
+//Comment here out
+//Get rought for notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
-  });
+});
+
+//
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json' ))
+});
+
+//Post notes information
+app.post('/notes', (req, res) => {
+    console.log(`${req.method} method request recieved`);
+    //Destructing the object properties in req.body
+    const { title, text } = req.body;
+    
+    // If all the required properties are present in user input
+    if (title && text) {
+        // Variable for the object we will save
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        }
+        //reading and appending (some content, 'to file path')
+        readAndAppend(newNote, './db/db.json');
+
+        //if all went well...
+        const response = {
+        status: 'success',
+        body: newNote,
+        };
+        //...then we log response
+        res.json(response);
+    } 
+    else {
+        //if all didnt go well
+        res.json('Error in posting new note');
+    }
+
+});
 
 //route listens to the port in terminal
 app.listen(PORT, () => {
